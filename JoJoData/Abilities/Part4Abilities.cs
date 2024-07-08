a740b1ce9d3d4fb65bd3573e01fba045f64529f5
Loguigi <ljm92@uakron.edu>
2024-07-08 19:52:40 -0400

    Add regions for part 4 abilities

---
+++ JoJoData/Abilities/Part4Abilities.cs
@@ -0,0 +1,113 @@
+using JoJoData.Library;
+using Barrier = JoJoData.Library.Barrier;
+using Random = JoJoData.Library.Random;
+
+namespace JoJoData.Abilities;
+
+#region Crazy Diamond
+// TODO
+#endregion
+
+#region The Hand
+// TODO
+#endregion
+
+#region Echoes Act 0
+
+#endregion
+
+#region Echoes Act 1
+
+#endregion
+
+#region Echoes Act 2
+
+#endregion
+
+#region Echoes Act 3
+
+#endregion
+
+#region Heaven's Door
+
+#endregion
+
+#region Killer Queen
+
+#endregion
+
+#region Aqua Necklace
+
+#endregion
+
+#region Bad Company
+
+#endregion
+
+#region Red Hot Chili Pepper
+
+#endregion
+
+#region The Lock
+
+#endregion
+
+#region Surface
+
+#endregion
+
+#region Love Deluxe
+
+#endregion
+
+#region Pearl Jam
+
+#endregion
+
+#region Achtung Baby
+
+#endregion
+
+#region Ratt
+
+#endregion
+
+#region Harvest
+
+#endregion
+
+#region Cinderella
+
+#endregion
+
+#region Atom Heart Father
+
+#endregion
+
+#region Boy II Man
+
+#endregion
+
+#region Earth Wind and Fire
+
+#endregion
+
+#region Highway Star
+
+#endregion
+
+#region Stray Cat
+
+#endregion
+
+#region Super Fly
+
+#endregion
+
+#region Enigma
+
+#endregion
+
+#region Cheap Trick
+
+#endregion
